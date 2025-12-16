4f7c205c5f1e0ca87784811eaca8f2b8c8ac6486
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2025-12-16 07:52:45 +0700

    fix: update database connection to use PostgreSQL and set default APP_PORT

--- Config/database.go
+++ Config/database.go
@@ -3,7 +3,8 @@ package Config
 import (
 	"fmt"
 	"os"
-	"gorm.io/driver/mysql"
+
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -11,14 +12,14 @@ var DB *gorm.DB
 
 func ConnectionDB(){
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
 	)
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Gagal terkoneksi: " + err.Error())
